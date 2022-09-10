ec06e587f18e0c6868e88f4b8442efe6a3f5f21a
Mohamed Ashraf <M.Ashraf@INTDV.com>
2022-09-11 01:41:49 +0200

    Add project files.

---
+++ Controllers/WeatherForecastController.cs
@@ -0,0 +1,36 @@
+using Microsoft.AspNetCore.Mvc;
+using WatchDog;
+
+namespace LogApplicationV2.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class WeatherForecastController : ControllerBase
+{
+    private static readonly string[] Summaries = new[]
+    {
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+    };
+
+    private readonly ILogger<WeatherForecastController> _logger;
+
+    public WeatherForecastController(ILogger<WeatherForecastController> logger)
+    {
+        _logger = logger;
+    }
+
+    [HttpGet(Name = "GetWeatherForecast")]
+    public IEnumerable<WeatherForecast> Get()
+    {
+        WatchLogger.Log("This is sample log from the app v1");
+        
+        //throw new Exception("Something went wrong, testing watchdog");
+        return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+        {
+            Date = DateTime.Now.AddDays(index),
+            TemperatureC = Random.Shared.Next(-20, 55),
+            Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+        })
+        .ToArray();
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,45 @@
+using WatchDog;
+using WatchDog.src.Enums;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddWatchDogServices(opt =>
+{
+    opt.IsAutoClear = false;
+    // opt.ClearTimeSchedule = WatchDogAutoClearScheduleEnum.Weekly;
+    opt.SetExternalDbConnString = builder.Configuration.GetConnectionString("SampleDbConnection");
+    opt.SqlDriverOption = WatchDogSqlDriverEnum.PostgreSql;
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+// inject into the middleware
+app.UseWatchDogExceptionLogger();
+
+app.UseWatchDog(opt =>
+{
+    opt.WatchPageUsername = "admin";
+    opt.WatchPagePassword = "12345678";
+});
+
+app.Run();
